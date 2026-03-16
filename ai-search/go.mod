f42158b297f005b0023f14117448644587bd152d
Bergomezz <bernardogomezz@yahoo.com.br>
2026-03-16 00:55:04 -0300

    FT0002 image generator / image.go file

--- ai-search/go.mod
+++ ai-search/go.mod
@@ -1,3 +1,7 @@
 module ai-search
 
 go 1.26.1
+
+require golang.org/x/image v0.37.0
+
+require github.com/StephaneBunel/bresenham v0.0.0-20211027152503-ec76d7b8e923 // indirect
---
+++ ai-search/image.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"os"
+
+	"github.com/StephaneBunel/bresenham"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+)
+
+const cellSize = 60
+
+var (
+	green = color.RGBA{G: 255, A: 255}
+	darkGreen = color.RGBA{R: 1, G: 100,B: 32, A: 255}
+	red =color.RGBA{R: 255, A: 255}
+	yellow =color.RGBA{R: 255,G: 255,B: 101, A: 255}
+	gray = color.RGBA{R: 125, G: 125, B: 125, A: 255}
+	orange = color.RGBA{R: 255, G: 140, B: 25, A: 255}
+	blue = color.RGBA{R: 14, G: 118, B: 173, A: 255}
+)
+
+func (g *Maze) OutputImage (fileName ...string) {
+	fmt.Printf("Generate image %s...\n", fileName)
+	width := cellSize * (g.Width -1)
+	height := cellSize * g.Height
+
+	var outFile = "image.png"
+	if len(fileName) > 0 {
+		outFile = fileName[0]
+	}
+
+	upLeft := image.Point{}
+	lowRight := image.Point{X: width, Y: height}
+
+	img := image.NewRGBA(image.Rectangle{Min: upLeft, Max: lowRight})
+
+	draw.Draw(img, img.Bounds(), &image.Uniform{C: color.Black}, image.Point{}, draw.Src)
+
+	for i, row := range g.Wall {
+		for j, col := range row {
+			p := Point{
+				Row: i,
+				Col: j,
+			}
+			if col.wall{
+				g.drawSquare(col, p, img, color.Black, cellSize, j*cellSize, i*cellSize)
+			}else if g.inSolution(p) {
+				g.drawSquare(col, p, img, green, cellSize, j*cellSize, i*cellSize)
+			}else if col.State.Row == g.Start.Row && col.State.Col == g.Start.Col {
+				g.drawSquare(col, p, img, darkGreen, cellSize, j*cellSize, i*cellSize)
+			}else if col.State.Row == g.Goal.Row && col.State.Col == g.Goal.Col {
+				g.drawSquare(col, p, img, red, cellSize, j*cellSize, i*cellSize)
+			}else if col.State == g.CurrentNode.State {
+				g.drawSquare(col, p, img, orange, cellSize, j*cellSize, i*cellSize)
+			}else if inExplored(Point{i, j}, g.Explored) {
+				g.drawSquare(col, p, img, yellow, cellSize, j*cellSize, i*cellSize)
+			}else {
+				g.drawSquare(col, p, img, color.White, cellSize, j*cellSize, i*cellSize)
+			}
+		}
+	}
+
+	for i, _ := range g.Wall {
+		bresenham.DrawLine(img, 0, i*cellSize, g.Width*cellSize, i*cellSize, gray)
+	}
+
+	for i := 0; i <= g.Width; i++ {
+		bresenham.DrawLine(img, i*cellSize, 0, i*cellSize, g.Height*cellSize, gray)
+	}
+
+	f, _ := os.Create(outFile)
+
+	_ = png.Encode(f, img)
+}
+
+
+func (g *Maze) drawSquare(col Wall, p Point, img *image.RGBA, c color.Color, size, x, y int) {
+	patch := image.NewRGBA(image.Rect(0, 0, size, size))
+	draw.Draw(
+		patch,
+		patch.Bounds(),
+		&image.Uniform{
+			C: c,
+		},
+		image.Point{},
+		draw.Src)
+	if !col.wall {
+		g.printLocation(p, color.Black, patch)
+	}
+
+	draw.Draw(img, image.Rect(x, y, x+size, y+size),patch, image.Point{}, draw.Src)
+}
+
+func (g *Maze) printLocation(p Point, c color.Color, patch *image.RGBA) {
+	point := fixed.Point26_6{X: fixed.I(6), Y: fixed.I(40)}
+	d := &font.Drawer{
+		Dst: patch,
+		Src: image.NewUniform(c),
+		Face: basicfont.Face7x13,
+		Dot: point,
+	}
+
+	d.DrawString(fmt.Sprintf("[%d %d]", p.Row, p.Col))
+}
